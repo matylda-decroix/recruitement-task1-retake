82260befa697d6e4eae4efb0dc89f57339348b0a
MATYLDA DECROIX <matylda.decroix@gmail.com>
2022-12-19 14:31:10 +0100

    Extending store state by apiData and game

---
+++ src/state/features/apiData/apiDataSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: Record<string, boolean> = {};
+
+export const apiDataSlice = createSlice({
+  name: "apiData",
+  initialState,
+  reducers: {},
+});
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiDataSlice } from "./features/apiData/apiDataSlice";
+import { gameSlice } from "./features/game/gameSlice";
 import { wordsSlice } from "./features/words/wordsSlice";
 
 export const store = configureStore({
   reducer: {
     words: wordsSlice.reducer,
+    game: gameSlice.reducer,
+    apiData: apiDataSlice.reducer,
   },
 });
 
